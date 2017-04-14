e832fd599c5334defcd90e17ab93d1db057e438e
yfsoftcom <yfsoftcom@126.com>
2017-04-14 16:42:14 +0800

    more types

--- src/upload.js
+++ src/upload.js
@@ -13,17 +13,23 @@ const router = Router()
 var datas = {}
 
 // 上传的文件类型限制
-const ALLOW_MIMETYPES = ['application/octet-stream', 'application/zip', 'application/x-zip-compressed']
+const ALLOW_MIMETYPES = [
+  'application/octet-stream', 
+  'application/json', 
+  'application/zip', 
+  'application/x-zip-compressed', 
+  'image/png', 
+  'image/jpeg']
 
 const fileFilter = (req, file, cb) =>{
   if(_.indexOf(ALLOW_MIMETYPES, file.mimetype)> -1){
     cb(null, 1)
   }else{
-    cb({ errno: -801, code: 'TYPE_NOT_ALLOWD', message: 'only zip, json accept'})
+    cb({ errno: -801, code: 'TYPE_NOT_ALLOWD', message: 'only images, zip, json accept'})
   }
 }
 
-const upload = multer({ dest: 'uploads/' , fileFilter: fileFilter, limits: { fileSize: 2 * 1024 * 1024 }})
+const upload = multer({ dest: 'uploads/' , fileFilter: fileFilter, limits: { fileSize: 5 * 1024 * 1024 }})
 
 // 上传表单以file为文件的字段
 const defaultHandler = upload.single('file')
@@ -32,13 +38,11 @@ const defaultHandler = upload.single('file')
 const handler = async (ctx, next) => {
   try{
     await defaultHandler(ctx, next)
-    console.log(ctx.req.file)
     let data = ctx.req.file
     datas[data.filename] = data
     let result = await sync(data)
     ctx.success({data: _.assign({id: data.filename}, result)})
   }catch(e){
-    console.log(e)
     ctx.fail(e)
   }
 }
